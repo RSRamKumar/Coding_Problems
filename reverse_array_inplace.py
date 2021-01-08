c3274d3045303502eb6f4aceeb780cc50a6d001f
Ram Kumar R S <39699070+RSRamKumar@users.noreply.github.com>
2021-01-09 00:17:52 +0100

    Add files via upload

--- reverse_array_inplace.py
+++ reverse_array_inplace.py
@@ -7,7 +7,7 @@ def reversing_array_inplace(array):
     back_index = len(array)-1
 
     while front_index < back_index:
-        print(array[front_index],array[back_index])
+       # print(array[front_index],array[back_index])
         array[front_index],array[back_index]=array[back_index],array[front_index]
         front_index +=1
         back_index -= 1
@@ -15,4 +15,20 @@ def reversing_array_inplace(array):
 
 print(reversing_array_inplace([1,2,3,4,5,6,7]))
 
+
+
+def reversing_array_inplace_using_forloop(array):
+    end = len(array) - 1
+    limit = int(end / 2) + 1
+    for i in range(limit):
+        array[i], array[end] = array[end], array[i]
+        end-=1
+    return array
+
+print(reversing_array_inplace_using_forloop([1,2,3,4,5,6,7]))
+print(reversing_array_inplace_using_forloop([1,2,3,4,5,6,7,8]))
+print(reversing_array_inplace([1,2,3,4,5,6,7]))
+
+
 #https://www.youtube.com/watch?v=p0VD9Fdlx5A
+#https://codereview.stackexchange.com/questions/155619/reversing-an-array-in-python
